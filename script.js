e090d31b919d24849e4569ebb74fcff763847637
ch1kim0n1 <chikimoni61@gmail.com>
2023-11-21 13:35:40 -0600

    day 9 + design

--- script.js
+++ script.js
@@ -1,77 +1,109 @@
+const base = "https://anilist.co/activity/";
+
 // Array of days 
 const days = [
     {
       day: 1, 
       label: "Day 1",
-      page:"https://anilist.co/activity/643310673",
+      page: base + "643310673",
       img: "images/AI/day1.jpg"
     },
     {
       day: 2,
       label: "Day 2",
-      page: "https://anilist.co/activity/644204461",
+      page: base + "644204461",
       img: "images/AI/day2.jpg"
     },
     {
     day: 3,
     label: "Day 3",
-    page: "https://anilist.co/activity/645119355",
+    page: base + "645119355",
     img: "images/AI/day3.jpg"
     },
     {
     day: 4,
     label: "Day 4",
-    page: "https://anilist.co/activity/645504758",
+    page: base + "645504758",
     img: "images/AI/day4.jpg"
     },
     {
     day: 5,
     label: "Day 5",
-    page: "https://anilist.co/activity/648821109",
+    page: base + "648821109",
     img: "images/AI/day5.jpg"
     },
     {
     day: 6,
     label: "Day 6",
-    page: "https://anilist.co/activity/648824064",
+    page: base + "648824064",
     img: "images/AI/day6.jpg"
     },
     {
     day: 7,
     label: "Day 7",
-    page: "https://anilist.co/activity/649128166",
+    page: base + "649128166",
     img: "images/AI/day7.jpg"
     },
     {
     day: 8,
     label: "Day 8",
-    page: "https://anilist.co/activity/649743957",
+    page: base + "649743957",
     img: "images/AI/day8.jpg"
     },
+    {
+      day: 9,
+      label: "Day 9",
+      page: base + "649983390",
+      img: "images/AI/day9.jpg"
+      },
   ];
   
-  // Get the days container
-  const daysContainer = document.querySelector('.days');
-  
-  // Loop through days
-  days.forEach(day => {
-  
-    // Create element
-    const dayEl = document.createElement('div');
-    dayEl.classList.add('day');
-  
-    // Add image
-    const img = document.createElement('img');
-    img.src = day.img;
-    dayEl.appendChild(img);
-  
-    // Add label 
-    const label = document.createElement('a');
-    label.href = day.page; 
-    label.textContent = day.label;
-    dayEl.appendChild(label);
-  
-    // Append to container
-    daysContainer.appendChild(dayEl);
-  
+ // Get days container 
+const daysContainer = document.querySelector('.days');
+
+// Loop through days
+days.forEach(day => {
+
+  // Create column div 
+  const column = document.createElement('div');
+  column.classList.add('day');
+
+  //image 
+  const img = document.createElement('img');
+  img.src = day.img;
+
+  //overlay
+  const overlay = document.createElement('div');
+  overlay.classList.add('overlay');
+
+  //title 
+  const title = document.createElement('h3');
+  title.textContent = day.label;
+  title.classList.add('custom-title');
+
+  // CUTTED FEATURE: SEPARATE LINKS
+  // const link = document.createElement('a');
+  // link.href = day.page;
+  // link.textContent = "View";
+  // link.classList.add("view-link");
+
+  // Append elements
+  column.appendChild(img);
+  overlay.appendChild(title);
+  //overlay.appendChild(link);
+  column.appendChild(overlay);
+
+  daysContainer.appendChild(column);
+
+  // Add click event listener to the image
+  img.addEventListener("click", () => {
+    window.location.href = day.page;
   });
+});
+
+// More button logic
+const moreBtn = document.querySelector(".more-btn");
+
+moreBtn.addEventListener("click", () => {
+  console.log("Show more"); 
+});
